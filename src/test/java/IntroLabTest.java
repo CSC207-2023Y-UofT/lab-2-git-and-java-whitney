463dc2add5a85b76b2ca7612b6c1cf14d595fab5
Shivesh Prakash <shiveshprakash2@gmail.com>
2023-05-18 18:59:32 -0400

    Added praka150() to IntroLab and relevant test in IntroLabTest

--- src/test/java/IntroLabTest.java
+++ src/test/java/IntroLabTest.java
@@ -30,4 +30,11 @@ public class IntroLabTest {
      *       the test results.
      * As a reference, we've included testExampleStudent above.
      */
+    @Test(timeout = 10)
+    public void testPraka150() {
+        String expected = "Hello! My name is Shivesh! " +
+                "I like cats more than dogs! Welcome to CSC207!";
+        String actual = IntroLab.praka150();
+        assertEquals(expected, actual);
+    }
 }
